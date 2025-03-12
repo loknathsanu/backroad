21ba0ae321a925f63226f4ff87db53545ec77d0e
loknathsanu <loknathkhadnga67@gmail.com>
2025-03-13 02:32:00 +0530

    Reworked entire project structure and updated code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import logo from "../images/logo.svg";
-import { socialLinks } from "../data";
+import { pageLinks, socialLinks } from "../data";
 import PageLinks from "./PageLinks";
-
+import SocialLink from "./SocialLink";
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -15,6 +15,12 @@ const Navbar = () => {
         {/* <!-- left this comment on purpose --> */}
 
         <PageLinks parentClass="nav-links" itemClass="nav-link" />
+
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            return <SocialLink {...link} key={link.id} itemClass="nav-icon" />;
+          })}
+        </ul>
       </div>
     </nav>
   );
